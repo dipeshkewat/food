26687b133b05140d47b76c70ca4a2e37488a844d
dipeshkewat <dipeshkewat8530@gmail.com>
2026-03-15 19:57:15 +0530

    created this food repo

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Use Google DNS to resolve MongoDB SRV records when local DNS fails
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
 
